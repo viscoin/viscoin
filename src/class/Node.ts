00b9a79dd9cbb852d9430dc1e2936cf8ec867e5d
larrencode <larrenliraren@gmail.com>
2020-10-12 20:10:56 +0200

    a

--- src/class/Node.ts
+++ src/class/Node.ts
@@ -2,23 +2,23 @@ import * as net from 'net'
 import * as crypto from 'crypto'
 import * as config from '../../config.json'
 interface Node {
-    dataHashes: Array<String>
+    dataHashes: Array<Buffer>
 }
 class Node {
     constructor() {
         this.dataHashes = []
     }
     onData(socket: net.Socket, data: Buffer) {
-        console.log(Node.verifyData(data))
-        const hash = crypto.createHash('sha256').update(data).digest('base64')
-        if (this.dataHashes.find(e => e === hash)) return
-        this.dataHashes.push(hash)
-        if (this.dataHashes.length > config.dataHashesLength) this.dataHashes.shift()
+        if (!this.verifyData(data)) return
         socket.write(data)
         console.log(data)
     }
-    static verifyData(data) {
-        if (Buffer.byteLength(data) > config.verifyData.maxSize) return false 
+    verifyData(data) {
+        if (Buffer.byteLength(data) > config.verifyData.maxByteLength) return false
+        const hash = crypto.createHash('sha256').update(data).digest()
+        if (this.dataHashes.find(e => e.compare(hash) === 0)) return false
+        this.dataHashes.push(hash)
+        if (this.dataHashes.length > config.dataHashesLength) this.dataHashes.shift()
         return true
     }
 }
