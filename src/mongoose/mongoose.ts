c3675e748604835e9288b4213bbe347dff26afd6
larrencode <larrenliraren@gmail.com>
2020-10-15 19:58:55 +0200

    a

--- src/mongoose/mongoose.ts
+++ src/mongoose/mongoose.ts
@@ -7,10 +7,10 @@ const client = {
         const dbOptions = {
             useNewUrlParser: true,
             autoIndex: false,
-            reconnectTries: Number.MAX_VALUE,
             poolSize: 5,
             connectTimeoutMS: 10000,
-            family: 4
+            family: 4,
+            useUnifiedTopology: true
         }
         
         mongoose.connect("mongodb://localhost:27017/js-coin", dbOptions)
@@ -18,7 +18,7 @@ const client = {
         mongoose.Promise = global.Promise
 
         mongoose.connection.on("connected", () => {
-            console.log("Mongoose connection successfully opened!")
+            // console.log("Mongoose connection successfully opened!")
         })
 
         mongoose.connection.on("err", err => {
