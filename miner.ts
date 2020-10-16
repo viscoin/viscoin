4ea5f739cc41ea2d030269781ef30e9a49562817
larrencode <larrenliraren@gmail.com>
2020-10-16 18:42:35 +0200

    a

--- miner.ts
+++ miner.ts
@@ -4,19 +4,39 @@ import * as mongoose from './src/mongoose/mongoose'
 mongoose.init()
 import Miner from './src/class/Miner'
 import * as keys from './keys.json'
-import Transaction from './src/class/Transaction'
+import * as config from './config.json'
+import * as cluster from 'cluster'
+import * as os from 'os'
 
-const miner = new Miner(keys[0].publicKey, true)
-const socket = miner.clientNode.createSocket('localhost', 8333)
-socket.on('connect', async () => {
-    await miner.load()
-    miner.start()
-    // miner.on('start', () => console.log('started mining'))
-    // miner.on('stop', () => console.log('stopped mining'))
-    // miner.on('null', data => console.log('received null', data))
-    // miner.on('block', data => console.log('received new block', data))
-    // miner.on('transaction', data => console.log('received new transaction', data))
-    miner.on('hash', (found, block) => {
-        if (found) console.log(block.height, block.hash)
+const init = () => {
+    const miner = new Miner(keys[0].publicKey, true)
+    const socket = miner.clientNode.createSocket('localhost', 8333)
+    socket.on('connect', async () => {
+        await miner.load()
+        miner.start()
+        // miner.on('start', () => console.log('started mining'))
+        // miner.on('stop', () => console.log('stopped mining'))
+        // miner.on('null', data => console.log('received null', data))
+        // miner.on('block', data => console.log('received new block', data))
+        // miner.on('transaction', data => console.log('received new transaction', data))
+        miner.on('hash', (found, block) => {
+            if (found) console.log(block.height, block.hash)
+        })
     })
-})
+}
+if (config.use.cluster) {
+    if (cluster.isMaster) {
+        for (let i = 0; i < os.cpus().length; i++) {
+            cluster.fork()
+                .on('exit', () => {
+                    cluster.fork()
+                })
+        }
+    }
+    else {
+        init()
+    }
+}
+else {
+    init()
+}
