8fb82272241bbf0bec19554d01a7fd52d3756109
larrencode <larrenliraren@gmail.com>
2020-10-18 18:48:21 +0200

    a

--- client.ts
+++ client.ts
@@ -7,10 +7,7 @@ import * as net from 'net'
 const clientNode = new ClientNode()
 for (const node of nodes) {
     const socket = clientNode.createSocket(node.port, node.address)
-    if (!socket) continue
-    socket.on('connect', () => {
-        socket.write(Buffer.from(Buffer.alloc(1, 0) + JSON.stringify(crypto.randomBytes(16))))
-    })
+    socket.on('connect', () => console.log('connected to socket :)'))
 }
 clientNode.on('data', data => {
     if (!clientNode.verifyData(data)) return
