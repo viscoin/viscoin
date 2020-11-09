6d112f0c9b4ac3559acd0aeb06a1e4674a61e399
larrencode <larrenliraren@gmail.com>
2020-11-09 22:40:33 +0100

    a

---
+++ worker.ts
@@ -0,0 +1,26 @@
+import { Worker, isMainThread, parentPort } from 'worker_threads'
+import { cpus } from 'os'
+import * as crypto from 'crypto'
+
+if (isMainThread) {
+    let i = 0
+    setInterval(() => {
+        console.log(`Hashrate: ${i} kH/s`)
+        i = 0
+    }, 1000)
+    for (const cpu of cpus()) {
+        const worker = new Worker(__filename)
+        worker.on('error', e => console.log('error', e))
+        worker.on('exit', e => console.log('exit', e))
+        worker.on('message', () => i++)
+        worker.on('messageerror', e => console.log('messageerror', e))
+        worker.on('online', () => console.log('online'))
+    }
+}
+else {
+    let i = 0
+    while (true) {
+        crypto.createHash('sha256').update('').digest()
+        if (i++ % 1000 === 0) parentPort.postMessage(null)
+    }
+}
