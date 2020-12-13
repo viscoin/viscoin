26ec6729d4b41ed153aa3be8845b1db64d3104d8
larrencode <larrenliraren@gmail.com>
2020-12-13 14:11:35 +0100

    a

--- miner.ts
+++ miner.ts
@@ -4,13 +4,15 @@ import Miner from './src/Miner'
 import { Worker, isMainThread, parentPort, threadId } from 'worker_threads'
 import MinerClient from './src/MinerClient'
 import base58 from './src/base58'
+import { setPriority } from 'os'
 
 if (isMainThread) {
     mongoose.init()
     const client = new MinerClient(base58.decode(config.miner.miningRewardAddress))
     client.on('mined', (block, code) => console.log('mined', block.height, 'code', code))
-    client.on('transaction', (transaction, code) => console.log('transaction', code))
+    // client.on('transaction', (transaction, code) => console.log('transaction', code))
     // client.on('block', (block, code) => console.log('block', code))
+    setPriority(19)
     for (let i = 0; i < client.threads; i++) {
         client.addWorker(new Worker(__filename))
     }
