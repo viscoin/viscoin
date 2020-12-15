229a79486a170b1caae1341b73eaa8e2b0f127b3
larrencode <larrenliraren@gmail.com>
2020-12-15 13:46:09 +0100

    a

--- src/keygen.ts
+++ src/keygen.ts
@@ -2,8 +2,14 @@ import * as crypto from 'crypto'
 import * as elliptic from 'elliptic'
 import addressFromPublicKey from './addressFromPublicKey'
 export default (seed: Buffer | undefined = undefined) => {
-    if (!seed) seed = crypto.randomBytes(32)
-    const privateKey = crypto.createHash('sha256').update(seed).digest()
+    if (seed) {
+        for (let i = 0; i < 10**6; i++) {
+            seed = crypto.createHash('sha256').update(seed).digest()
+            if (i % 10**5 === 0) console.log(i / 10**4, '%')
+        }
+    }
+    else seed = crypto.randomBytes(32)
+    const privateKey = seed
     const ec = new elliptic.ec('secp256k1')
     const key = ec.keyFromPrivate(privateKey)
     const pubPoint = key.getPublic()
