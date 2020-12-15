ad89c8bfd8efae4b4ae9a85381a9d741b858af47
larrencode <larrenliraren@gmail.com>
2020-12-15 20:34:38 +0100

    a

--- src/keygen.ts
+++ src/keygen.ts
@@ -1,8 +1,10 @@
 import * as crypto from 'crypto'
 import * as elliptic from 'elliptic'
 import addressFromPublicKey from './addressFromPublicKey'
+import { setPriority } from 'os'
 export default (seed: Buffer | undefined = undefined) => {
     if (seed) {
+        setPriority(19)
         for (let i = 0; i < 10**6; i++) {
             seed = crypto.createHash('sha256').update(seed).digest()
             if (i % 10**4 === 0) console.log(i / 10**4, '%')
