4550fb7e9c7ef1a80f83fe5b19a5e9b428522266
larrencode <larrenliraren@gmail.com>
2020-12-15 19:08:21 +0100

    a

---
+++ validatefiles.ts
@@ -0,0 +1,13 @@
+import * as fs from 'fs'
+import * as crypto from 'crypto'
+
+const files = [
+    ...fs.readdirSync('./').map(e => e = `./${e}`),
+    ...fs.readdirSync('./src').map(e => e = `./src/${e}`)
+].filter(e => e.endsWith('.js'))
+console.log(files)
+let data = Buffer.alloc(0)
+for (const file of files) {
+    data = Buffer.concat([ data, fs.readFileSync(file) ])
+}
+console.log(crypto.createHash('sha256').update(data).digest('hex'))
