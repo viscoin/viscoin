df9338aac44895c2c1c6df7c14224d7a722f0a8b
larrencode <larrenliraren@gmail.com>
2020-12-23 00:07:52 +0100

    a

--- src/Blockchain.ts
+++ src/Blockchain.ts
@@ -5,6 +5,7 @@ import model_block from './mongoose/model/block'
 import parseBigInt from './parseBigInt'
 import beautifyBigInt from './beautifyBigInt'
 import model_address from './mongoose/model/address'
+import * as events from 'events'
 interface Blockchain {
     difficulty: number
     pendingTransactions: Array<Transaction>
@@ -13,12 +14,15 @@ interface Blockchain {
     blockHashes: Array<Buffer>
     oldHashes: Array<Buffer>
     newHashes: Array<Buffer>
+    updatingBlockHashes: boolean
 }
-class Blockchain {
+class Blockchain extends events.EventEmitter {
     constructor() {
+        super()
         this.difficulty = 0
         this.pendingTransactions = []
         this.syncIndex = 0
+        this.updatingBlockHashes = false
     }
     async setBlockHashes() {
         this.blockHashes = []
@@ -32,6 +36,10 @@ class Blockchain {
         if (!this.blockHashes) await this.setBlockHashes()
         if (!this.oldHashes) this.oldHashes = []
         if (!this.newHashes) this.newHashes = []
+        if (this.updatingBlockHashes) {
+            return await new Promise(resolve => this.once('updated-block-hashes', () => resolve()))
+        }
+        this.updatingBlockHashes = true
         let block = await this.getLatestBlock(),
         index: number = null,
         done: boolean = false
@@ -54,6 +62,8 @@ class Blockchain {
         this.blockHashes = this.blockHashes.slice(0, index)
         this.blockHashes.push(...newHashes)
         this.newHashes = newHashes
+        this.emit('updated-block-hashes')
+        this.updatingBlockHashes = false
     }
     createGenesisBlock() {
         return new Block({
@@ -160,18 +170,21 @@ class Blockchain {
         return 0
     }
     async getTransactionsOfAddress(address: Buffer, projection: string | null = null, optimization: boolean = false) {
+        await this.updateBlockHashes()
         // !
-        if (!this.oldHashes
-        || !this.newHashes) await this.updateBlockHashes()
-        // !
-        // await this.updateBlockHashes()
+        // if (!this.blockHashes
+        // || !this.oldHashes
+        // || !this.newHashes) await this.updateBlockHashes()
         let blocks = [],
         old_blocks = []
         const baseQuery = {
             $or: [
                 { [`${config.block.transactions.name}.${config.transaction.to.name}`]: address.toString('binary') },
                 { [`${config.block.transactions.name}.${config.transaction.from.name}`]: address.toString('binary') }
-            ]
+            ],
+            [config.block.hash.name]: {
+                $in: this.blockHashes.map(e => e.toString('binary'))
+            }
         }
         if (optimization) {
             old_blocks = await Block.loadMany({
@@ -193,7 +206,6 @@ class Blockchain {
         const getTransactions = (blocks: Array<{ hash: Buffer, transactions: Array<{ from: Buffer | undefined, to: Buffer | undefined, timestamp: number | undefined }>, timestamp: number | undefined }>) => {
             const transactions = []
             for (const block of blocks) {
-                if (!this.blockHashes.find(e => e.equals(block.hash))) continue
                 for (const transaction of block.transactions) {
                     if ((transaction.from && address.equals(transaction.from))
                         || (transaction.to && address.equals(transaction.to))) {
