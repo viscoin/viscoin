03bbc0b4b397c7a9804db98f57a9203ffb4b757b
larrencode <larrenliraren@gmail.com>
2020-12-23 15:58:21 +0100

    a

---
+++ index.ts
@@ -0,0 +1,8 @@
+import BaseClient from './src/BaseClient'
+import MinerClient from './src/MinerClient'
+import WalletClient from './src/WalletClient'
+export default {
+    BaseClient,
+    MinerClient,
+    WalletClient
+}
