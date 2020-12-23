3c89d4c701aa026b06aead735e6a2fa4ea9ea5bc
larrencode <larrenliraren@gmail.com>
2020-12-23 20:32:52 +0100

    a

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
 import BaseClient from './src/BaseClient'
 import MinerClient from './src/MinerClient'
 import WalletClient from './src/WalletClient'
+import Blockchain from './src/Blockchain'
+import Block from './src/Block'
+import Transaction from './src/Transaction'
+import base58 from './src/base58'
 export default {
     BaseClient,
     MinerClient,
-    WalletClient
+    WalletClient,
+    Blockchain,
+    Block,
+    Transaction,
+    base58
 }
