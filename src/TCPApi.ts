2a709023aa2cfe31174473d5fd617ffa22abba99
larrencode <larrenliraren@gmail.com>
2020-12-30 21:47:12 +0100

    a

--- src/TCPApi.ts
+++ src/TCPApi.ts
@@ -2,6 +2,9 @@ import * as net from 'net'
 import * as events from 'events'
 import * as config from '../config.json'
 import protocol from './protocol'
+interface Socket extends net.Socket {
+    data: Buffer
+}
 interface Server {
     sockets: Set<net.Socket>
     server: net.Server
@@ -14,6 +17,7 @@ class Server extends events.EventEmitter {
         this.server.maxConnections = config.TCPApi.maxConnections
         this.server.on('connection', (socket: net.Socket) => {
             this.sockets.add(socket)
+            socket.on('error', () => {})
             socket.on('close', () => this.sockets.delete(socket))
             socket.on('data', () => socket.destroy())
         })
@@ -40,16 +44,24 @@ class Client extends events.EventEmitter {
         this.sockets = new Set()
     }
     connect(port: number, host: string) {
-        const socket = net.connect(port, host)
+        const socket = <Socket> net.connect(port, host)
+        socket.data = Buffer.alloc(0)
         socket.on('connect', () => {
             this.sockets.add(socket)
         })
+        socket.on('error', () => {})
         socket.on('close', () => this.sockets.delete(socket))
-        socket.on('data', buffer => {
-            const parsed = protocol.parse(buffer)
-            if (parsed === null) return
-            const { type, data } = parsed
-            this.emit(type, data)
+        socket.on('data', chunk => {
+            socket.data = Buffer.concat([ socket.data, chunk ])
+            let index = protocol.getEndIndex(socket.data)
+            while (index !== -1 && !socket.destroyed) {
+                const parsed = protocol.parse(socket.data.slice(0, index))
+                socket.data = socket.data.slice(index + 32)
+                if (parsed === null) continue
+                const { type, data } = parsed
+                this.emit(type, data)
+                index = protocol.getEndIndex(socket.data)
+            }
         })
     }
 }
