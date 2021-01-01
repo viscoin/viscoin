268c0f098e0c9b98ed3458e416284a4baa8a8f1b
larrencode <larrenliraren@gmail.com>
2021-01-01 19:04:57 +0100

    a

--- src/customHash.ts
+++ src/customHash.ts
@@ -1,17 +1,8 @@
 import * as crypto from 'crypto'
 const hashes = [
-    'sha3-512',
-    'sha512',
-    'whirlpool',
-    'blake2s256'
+    'blake2b512',
+    'sha256'
 ]
-// const hashes = [
-//     'sha3-512',
-//     'sha512',
-//     'whirlpool',
-//     'blake2b512',
-//     'shake256'
-// ]
 export default (data) => {
     for (const hash of hashes) {
         data = crypto.createHash(hash).update(data).digest()
