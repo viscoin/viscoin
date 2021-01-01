f756965a5da6183f5283be10a08c51b2637673c8
larrencode <larrenliraren@gmail.com>
2021-01-01 22:15:08 +0100

    a

--- src/customHash.ts
+++ src/customHash.ts
@@ -9,3 +9,17 @@ export default (data) => {
     }
     return data
 }
+// import * as crypto from 'crypto'
+// const length = 20,
+// N = Math.pow(2, 16),
+// r = 8
+// const maxmem = 2 * 128 * N * r
+// export default (data) => {
+//     const password = crypto.createHash('blake2b512').update(data).digest()
+//     const salt = crypto.createHash('sha256').update(password).digest()
+//     return crypto.scryptSync(password, salt, length, {
+//         N,
+//         r,
+//         maxmem
+//     })
+// }
