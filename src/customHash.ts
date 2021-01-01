27b92b4790fd44fd1255aa2dbb16471cd73152a0
larrencode <larrenliraren@gmail.com>
2021-01-01 17:31:15 +0100

    a

--- src/customHash.ts
+++ src/customHash.ts
@@ -2,13 +2,8 @@ import * as crypto from 'crypto'
 const hashes = [
     'sha3-512',
     'sha512',
-    'blake2b512',
     'whirlpool',
-    'sha3-384',
-    'sha384',
-    'sha3-256',
-    'sha256',
-    'blake2s256',
+    'blake2b512',
     'shake256'
 ]
 export default (data) => {
