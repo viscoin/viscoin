2afb20819a22d6baa5817d4998908c72ad5145b1
larrencode <larrenliraren@gmail.com>
2021-01-02 16:19:50 +0100

    a

--- src/addressFromPublicKey.ts
+++ src/addressFromPublicKey.ts
@@ -1,6 +1,4 @@
-import * as crypto from 'crypto'
+import customHash from './customHash'
 export default (publicKey: Buffer) => {
-    const a = crypto.createHash('sha256').update(publicKey).digest()
-    const b = crypto.createHash('sha3-256').update(a).digest()
-    return Buffer.concat([a.slice(-10), b.slice(-10)])
+    return customHash(publicKey).slice(-20)
 }
