70754f246078ab1a0c3727b4806df470536ba5c6
larrencode <larrenliraren@gmail.com>
2021-01-06 21:11:02 +0100

    a

--- src/customHash.ts
+++ src/customHash.ts
@@ -1,12 +1,17 @@
 import * as crypto from 'crypto'
-const hashes = [
-    'blake2b512',
-    'sha3-512',
-    'sha256'
-]
+const salt = Buffer.alloc(0)
+const length = 20
+const N = 1024
+const r = 8
+const p = 1
+const maxmem = 256 * N * r
+const options = {
+    N,
+    r,
+    p,
+    maxmem
+}
 export default (data) => {
-    for (const hash of hashes) {
-        data = crypto.createHash(hash).update(data).digest()
-    }
-    return data
+    const password = crypto.createHash('blake2b512').update(data).digest()
+    return crypto.scryptSync(password, salt, length, options)
 }
