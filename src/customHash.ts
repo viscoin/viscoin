8d4db7743db0f5991cc1e599bb2e33062804f34c
larrencode <larrenliraren@gmail.com>
2021-01-10 15:23:36 +0100

    a

--- src/customHash.ts
+++ src/customHash.ts
@@ -14,20 +14,3 @@ options = {
 export default async (data) => {
     return await hash(crypto.createHash('blake2b512').update(data).digest(), salt, options)
 }
-// import * as crypto from 'crypto'
-// const salt = Buffer.alloc(0)
-// const keylen = 32
-// const N = 1024
-// const r = 8
-// const p = 1
-// const maxmem = 256 * N * r
-// const options = {
-//     N,
-//     r,
-//     p,
-//     maxmem
-// }
-// export default (data) => {
-//     const password = crypto.createHash('blake2b512').update(data).digest()
-//     return crypto.scryptSync(password, salt, keylen, options)
-// }
